8b74a8ff6cab58b35e063ff2154e6be690ea6a77
MichelleChou1618 <starresttmichelle@gmail.com>
2023-08-11 19:55:16 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV !== 'production') {
 //載入 mongoose
 const mongoose = require('mongoose')
 // 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -87,6 +87,29 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
      .then((restaurant) => res.render('show', { restaurant: restaurant })) //資料會被存在restaurant 變數裡，傳給樣板引擎，請 Handlebars 幫忙組裝 show 頁面
      .catch(error => console.log(error))
 })
+
+//設定首頁, Show頁面 - 點擊'Edit' button - 路由: 至Edit頁面: 表單with 預設資料 
+app.get('/restaurants/:restaurant_id/edit', (req, res) => {
+  const id = req.params.restaurant_id //從req.params取出動態路由裡的id
+  return Restaurant.findById(id) //至資料庫用id查詢特定一筆 restaurant 資料 => Restaurant.findById()
+    .lean()               //轉換成乾淨的 JavaScript 資料物件
+    .then((restaurant) => res.render('edit', { restaurant })) //資料會被存在 restaurant 變數裡，傳給樣板引擎，請 Handlebars 幫忙組裝 edit 頁面
+    .catch(error => console.log(error))
+})
+
+  // 設定Edit頁面 - 點擊'儲存修改' button - 路由: Update一筆restaurant
+  app.post('/restaurants/:restaurant_id/edit', (req, res) => {
+    const id = req.params.restaurant_id   //從req.params取出動態路由裡的id 
+    //const newRestaurant = req.body // 從 req.body 拿出表單裡資料
+    // return Restaurant.findById(id)   //至資料庫用id查詢特定一筆 restaurant 資料 => Todo.findById()
+    //   .then(restaurant => {          // 如果查詢成功, 修改後儲存資料
+    //     restaurant = newRestaurant
+    //     return restaurant.save()
+    //   })
+    return Restaurant.findByIdAndUpdate(id, req.body)
+      .then(() => res.redirect(`/restaurants/${id}`)) //如果儲存成功, 導回Show頁面
+      .catch(error => console.log(error))      //任一步驟出現失敗，都會跳進錯誤處理
+  })
 
 app.get('/search', (req, res) => {
   //console.log(req.query)
