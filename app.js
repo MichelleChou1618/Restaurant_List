3fe29b76863013a632c2107b46e71f9b20e4bf98
MichelleChou1618 <starresttmichelle@gmail.com>
2023-08-11 17:45:59 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -9,12 +9,34 @@ const exphbs = require('express-handlebars')
 //require restaurant.json here
 const restaurantList = require('./restaurant.json')
 
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+//載入 mongoose
+const mongoose = require('mongoose')
+//// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting static files
 app.use(express.static('public'))
+
+
 
 // routes setting
 app.get('/', (req, res) => {
